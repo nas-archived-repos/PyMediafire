44caa948890dad9c48cd7cbeeac64d4c6b529b71
Stepan Kuznetsov <superstepa@yandex.ru>
2013-01-28 23:41:17 -0800

    Put everything in a class

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import requests,hashlib,getpass,json
-
 email =""
 password = ""
 application_id = ""
 api_key = ''
-response_format = "json"
-def Authenticate(email,password,application_id,response_format):
-	signature = hashlib.sha1(email+password+application_id+api_key).hexdigest()
-	parameters = {'email':email,'password':password,'application_id':application_id,'signature':signature,'response_format':response_format}
-	r = requests.get("https://www.mediafire.com/api/user/get_session_token.php",params=parameters)
-	json = r.json()
-	json = json['response']
-	if (json['result'] == "Error"):
-		return json["message"]
-	else:
-		session_token = json['session_token']
-		return session_token
+response_format = ""
+class Mediafire:
+	def __init__(self,email,password,application_id,api_key,response_format):
+		self.email = email
+		self.password = password
+		self.application_id = application_id
+		self.api_key = api_key
+		self.response_format = response_format
 
-x = Authenticate(email,password,application_id,response_format)
-print x
+	def Authenticate(self):
+		signature = hashlib.sha1(self.email+self.password+self.application_id+self.api_key).hexdigest()
+		parameters = {'email':self.email,'password':self.password,'application_id':self.application_id,'signature':signature,'response_format':self.response_format}
+		r = requests.get("https://www.mediafire.com/api/user/get_session_token.php",params=parameters)
+		json = r.json()
+		json = json['response']
+		if (json['result'] == "Error"):
+			return json["message"]
+		else:
+			session_token = json['session_token']
+			return session_token
+
