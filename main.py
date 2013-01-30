e5a3eb8d3682194e065b25368b94302ae77aba66
superstepa <superstepa@yandex.ru>
2013-01-29 18:51:53 -0800

    Added some functions related to the account

--- main.py
+++ main.py
@@ -2,25 +2,105 @@ import requests,hashlib,getpass,json
 email =""
 password = ""
 application_id = ""
-api_key = ""
-response_format = ""
+api_key = ''
+response_format = "json"
 class Mediafire:
-	def __init__(self,email,password,application_id,api_key,response_format):
+	def __init__(self,email,password,application_id,api_key,response_format="json"):
 		self.email = email
 		self.password = password
 		self.application_id = application_id
 		self.api_key = api_key
 		self.response_format = response_format
+		self.signature = hashlib.sha1(self.email+self.password+self.application_id+self.api_key).hexdigest()
+		
+		self.login_url = "http://www.mediafire.com/api/user/login_with_token.php?login_token="
+
 
 	def get_session_token(self):
-		signature = hashlib.sha1(self.email+self.password+self.application_id+self.api_key).hexdigest()
-		parameters = {'email':self.email,'password':self.password,'application_id':self.application_id,'signature':signature,'response_format':self.response_format}
+		parameters = {'email':self.email,'password':self.password,'application_id':self.application_id,'signature':self.signature,'response_format':self.response_format}
 		r = requests.get("https://www.mediafire.com/api/user/get_session_token.php",params=parameters)
-		json = r.json()
-		json = json['response']
+		json = r.json()['response']
 		if (json['result'] == "Error"):
-			return json["message"]
+			print json["message"]
 		else:
-			session_token = json['session_token']
-			return session_token
+			self.session_token = json['session_token']
 
+	def renew_session_token(self):
+		parameters = {'session_token':self.session_token,'response_format':self.response_format}
+		r = requests.get("http://www.mediafire.com/api/user/renew_session_token.php",params=parameters)
+		json = r.json()['response']
+		if (json['result'] == "Error"):
+			print json["message"]
+		else:
+			self.session_token = json['session_token']
+
+	def get_login_token(self):
+		parameters = {'email':self.email,'password':self.password,'application_id':self.application_id,'signature':self.signature,'response_format':self.response_format}
+		r = requests.get("https://www.mediafire.com/api/user/get_login_token.php",params=parameters)
+		json = r.json()['response']
+		if (json['result'] == "Error"):
+			print json["message"]
+		else:
+			self.login_token = json['login_token']
+			self.login_url = "http://www.mediafire.com/api/user/login_with_token.php?login_token=" + self.login_token
+
+	def register(self,email,password,first_name,last_name,display_name): #Does not work yet
+		parameters = {'application_id':self.application_id,'email':email,'password':password,'response_format':self.response_format}
+		r = requests.get("https://www.mediafire.com/api/user/register.php",params = parameters)
+		json = r.json()["response"]
+		if (json['result'] == "Error"):
+			print json["message"]
+		else:
+			print "Registered user for " + email + " succesfully"
+
+	def get_info(self):
+		parameters = {'session_token':self.session_token,'response_format':self.response_format}
+		r = requests.get("http://www.mediafire.com/api/user/get_info.php",params=parameters)
+		json = r.json()['response']
+		if (json['result'] == 'Error'):
+			print json['message']
+		else:
+			json = json['user_info']
+			return json
+	def update(self,display_name,first_name,last_name,birth_date,gender,website,location,newsletter,primary_usage):
+		parameters = {'session_token':self.session_token,'response_format':self.response_format,'display_name':display_name,'first_name':first_name,'last_name':last_name,'birth_date':birth_date,'gender':gender,'website':website,'location':location,'newsletter':newsletter,'primary_usage':primary_usage}
+		r = requests.get("http://www.mediafire.com/api/user/update.php",params=parameters)
+		json = r.json()['response']
+		if (json['result'] == 'Error'):
+			print json['message']
+		else:
+			print "Updated succesfully"
+
+	def myfiles_revision(self): #Still have to figure out how this works
+		parameters = {'session_token':self.session_token,'response_format':self.response_format}
+		r = requests.get("http://www.mediafire.com/api/user/myfiles_revision.php",params=parameters)
+		json = r.json()['response']
+		if (json['result'] == 'Error'):
+			print json['message']
+		else:
+			return json['revision']
+
+	def fetch_tos(self): #I should find a way to display the actual terms of service
+		parameters = {'session_token':self.session_token,'response_format':self.response_format}
+		r = requests.get("http://www.mediafire.com/api/user/fetch_tos.php", params = parameters)
+		json = r.json()['response']
+		if (json['result'] == 'Error'):
+			print json['message']
+		else:
+			self.acceptance_token = json["terms_of_service"]["acceptance_token"]
+
+	def accept_tos(self):
+		parameters = {'session_token':self.session_token,'acceptance_token':self.acceptance_token,'response_format':self.response_format}
+		r = requests.get("http://www.mediafire.com/api/user/accept_tos.php", params = parameters)
+		json = r.json()['response']
+		if (json['result'] == 'Error'):
+			print json['message']
+		else:
+			print "Accepted the TOS succesfully"
+
+	#File functions
+			
+	 
+
+mediafire = Mediafire(email,password,application_id,api_key,response_format)
+mediafire.get_session_token()
