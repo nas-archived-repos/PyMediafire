4115cc7c1a92bf9b9bab04125f4f94743286b00a
superstepa <superstepa@yandex.ru>
2013-04-20 20:18:27 -0700

    Using __init__ properly now

--- example.py
+++ example.py
@@ -1,11 +1,9 @@
-from mediafire.mediafire_user import Mediafire_User
-
+from mediafire import user
 email =""
 password = ""
 application_id = ""
 api_key = ""
 
 
-test_user = Mediafire_User(email,password,application_id,api_key)
+test_user = mediafire.Mediafire_User(email,password,application_id,api_key)
 test_user.get_session_token()
-print(test_user.get_content()[0].get_info())
---
+++ mediafire/__init__.py
@@ -0,0 +1,3 @@
+from mediafire_user import Mediafire_User
+from mediafire_file import Mediafire_File
+from mediafire_folder import Mediafire_Folder
