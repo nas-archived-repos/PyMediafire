25fd127c55958232fcd40014366ef11e19096329
Nielson Santana <nielsonnas@gmail.com>
2013-09-10 00:56:59 -0300

    adding setup config

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="django-mediafire",
+           version="0.1",
+           description="python wrapper for mediafire storage service",
+           author="Nielson Santana",
+           author_email="nielsonnas@gmail.com",
+           packages=find_packages(),
+           include_package_data=True,
+)
+
